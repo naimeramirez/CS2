bac7c9f28085c0a11912a28731e8f980169f7714
naimeramirez <127249348+naimeramirez@users.noreply.github.com>
2024-03-08 20:20:00 -0700

    nodes

--- Node.java
+++ Node.java
@@ -1,14 +1,21 @@
-public class Node {
+class Node {
     String data;
     Node next;
 
-    public Node (String data, Node next) {
+    public Node(String data) {
         this.data = data;
-        this.next = next;
     }
 
-    public Node (String data) {
-        this(data, null);
+    public String toString() {
+        String result = "";
+        Node current = this;
+        while (current != null) {
+            result += current.data;
+            if (current.next != null) {
+                result += " -> ";
+            }
+            current = current.next;
+        }
+        return result;
     }
-
 }
