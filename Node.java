ae51261ee3b06d03212f9db97fadca629f4c1f32
naimeramirez <127249348+naimeramirez@users.noreply.github.com>
2024-03-08 18:31:17 -0700

    nodes

---
+++ Node.java
@@ -0,0 +1,14 @@
+public class Node {
+    String data;
+    Node next;
+
+    public Node (String data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    public Node (String data) {
+        this(data, null);
+    }
+
+}
---
+++ NodeTester.java
@@ -0,0 +1,9 @@
+public class NodeTester {
+    public static void main (String[] args) {
+        Node n1 = new Node("A");
+        Node n2 = new Node("B");
+        Node n3 = new Node("C");
+        Node n4 = new Node("D");
+        Node n5 = new Node("E");
+    }
+}
